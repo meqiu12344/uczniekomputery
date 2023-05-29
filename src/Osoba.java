a14c82726481693f45caabb137323010754da388
Mateusz <hola@gmail.com>
2023-05-29 09:29:27 +0200

    Initial commit

---
+++ src/Osoba.java
@@ -0,0 +1,33 @@
+public class Osoba {
+    private String imie, nazwisko, pesel;
+
+    public Osoba(String imie, String nazwisko, String pesel) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.pesel = pesel;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+}
---
+++ src/Uczen.java
@@ -0,0 +1,11 @@
+public class Uczen {
+    private String klasa;
+    private Boolean czyZdaje;
+
+
+
+    public Uczen(String klasa, Boolean czyZdaje) {
+        this.klasa = klasa;
+        this.czyZdaje = czyZdaje;
+    }
+}
